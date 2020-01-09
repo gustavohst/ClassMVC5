0d9226be1934b2fd10c9541de07c8aea7866ff31
Gustavo Tenório <gustavohst@msn.com>
2020-01-08 22:31:39 -0300

    Implment Views data and ajust views

--- Introducao/Controllers/HomeController.cs
+++ Introducao/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Introducao.Models;
 using System.Web.Mvc;
 
 namespace Introducao.Controllers
@@ -10,11 +7,46 @@ namespace Introducao.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+
+            /*
+              This whay is not recomended to use in MVC because is not clean. 
+                var pessoa2 = new Pessoa();
+                pessoa2.PessoaId = 1;
+                pessoa2.Nome = "Gustavo Tenório";
+                pessoa2.Tipo = "Administrador";
+            */
+            var pessoa = new Pessoa
+            {
+                PessoaId = 1,
+                Nome = "Gustavo Tenório",
+                Tipo = "Administrador"
+            };
+
+            /*  Associating data with view
+            ViewData["PessoaId"] = pessoa.PessoaId;
+            ViewData["Nome"] = pessoa.Nome;
+            ViewData["Tipo"] = pessoa.Tipo;
+            */       
+            /* Associating data with view bag
+            ViewBag.id = pessoa.PessoaId;
+            ViewBag.nome = pessoa.Nome;
+            ViewBag.tipo = pessoa.Tipo;
+            */
+            return View(pessoa);
         }
 
         public ActionResult Contatos()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Lista(int PessoaId, string Nome, string Tipo)
+        {
+            ViewData["PessoaId"] = PessoaId;
+            ViewData["Nome"] = Nome;
+            ViewData["Tipo"] = Tipo;
+            
             return View();
         }
 
---
+++ Introducao/Models/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Introducao.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+    }
+}
